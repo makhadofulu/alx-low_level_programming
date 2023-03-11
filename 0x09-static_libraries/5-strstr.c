d180509d29d242bd0b7b941185044ac69ae7c69c
Makhado <makhadofulufhelo@gmai.com>
2023-03-10 18:10:53 -0800

    libmy

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  * _strstr - Entry point
  * @haystack: input
  * @needle: input
  * Return: Always 0 (Success)
  */
+
 char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
